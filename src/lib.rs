e69994941ae7facac9475710035a8db38ab8e681
Ross MacArthur <ross@macarthur.io>
2020-08-31 10:53:55 +0200

    Add `regex!` and `try_regex!` macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,35 @@
+/// Generate a static regex.
+#[macro_export]
+macro_rules! regex {
+    ($re:expr $(,)?) => {{
+        static RE: ::once_cell::sync::OnceCell<regex::Regex> = ::once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| ::regex::Regex::new($re).unwrap())
+    }};
+}
+
+/// Try generate a static regex.
+#[macro_export]
+macro_rules! try_regex {
+    ($re:expr $(,)?) => {{
+        static RE: ::once_cell::sync::OnceCell<regex::Regex> = ::once_cell::sync::OnceCell::new();
+        RE.get_or_try_init(|| ::regex::Regex::new($re))
+    }};
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn regex() {
+        let hex = regex!("[0-9a-f]+");
+        assert!(hex.is_match("deadbeef"));
+    }
+
+    #[test]
+    fn try_regex() -> Result<(), regex::Error> {
+        let hex = try_regex!("[0-9a-f]+")?;
+        assert!(hex.is_match("deadbeef"));
+        Ok(())
     }
 }
